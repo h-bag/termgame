5e026cad102ad2c68a420f24dd739835198f41ca
h-bag <30416854+h-bag@users.noreply.github.com>
2021-10-07 23:37:41 +0100

    Add basic movement & other shit

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 // all actual game code goes here
+// sort of an entry point for all other scripts, as this is what's ran whenever the player submits
+// ANY command - it's all handled by the parseCommand function
 
 // define output var
 let output = String;
@@ -10,26 +12,47 @@ const player = {
     energy:100,
     hunger:100,
     money:50,
-    coords:[0,0,0]
-};
+    coords:[0,0,0] // x, y, z respectively
+};                 // north - x goes up, south - x goes down, east - z goes up, west - z goes down
+                   // im sure you can figure out y; up is up, down is down. obviously.
 
 
 // entry point for all player commands, essentially a "hub" for that shit i guess
 function parseCommand(cmd) {
     getCurrentRoom(); // defined in rooms.js
-    switch(cmd) {
+    switch(splitCmd(cmd)[0].toLowerCase()) {
         case "help":
-            output = "lol. lmao.<br>";
+            output = "lol. lmao.";
             break;
         case "look":
             output = (current_room.name + '<br>' + current_room.desc);
             break;
+        case "go":
+            goCmd(splitCmd(cmd)[1]);
+            break;
         // if none of the commands match
         default:
-            output = "That makes no sense. Try using 'HELP'.<br>";
+            output = "That makes no sense. Try using 'HELP'.";
     }
 
     // print output shit to user's screen
     output = ('<p style="color:white;margin:0">&gt;' + cmd.toUpperCase() + '</p>' + output)
     document.getElementById("outputBox").innerHTML += output;
 }
+
+function splitCmd(cmd) {
+    cmdFirstWord = cmd.split(' ')[0];
+    cmdOtherWords = cmd.replace(cmdFirstWord + ' ', '');
+    return [cmdFirstWord,cmdOtherWords];
+}
+
+function isEqual(arr1, arr2) {
+	// Check if the arrays are the same length
+	if (arr1.length !== arr2.length) return false;
+	// Check if all items exist and are in the same order
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) return false;
+	}
+	// Otherwise, return true
+	return true;
+};
--- scripts/rooms.js
+++ scripts/rooms.js
@@ -8,14 +8,26 @@ class Room{
         this.desc = desc;
         this.exits = exits;
         this.coords = coords;
+
+        // functions to be ran upon object creation
+        this.addToArray = function() {
+            rooms.push(this);
+        }
+        this.addToArray(); // add room object to array of rooms
     }
 }
 
-
-let player_apartment = new Room('Your Apartment','uhhh',null,[0,0,0]);
-
 // init rooms array
-let rooms = [player_apartment]
+var rooms = [];
+
+// maybe store room descs in seperate files, so they dont take up shitloads of space?
+let player_apartment = new Room('Your Apartment',
+                                'uhhh',
+                                ['south'],
+                                [0,0,0]);
+
+let devroom = new Room('Dev Room', "You shouldn't be here.", ['north'], [-1,0,0]);
+
 
 // set starting room
 let current_room = player_apartment;
@@ -27,8 +39,47 @@ let current_room = player_apartment;
 
 function getCurrentRoom() {
     for (let i = 0; i < rooms.length; i++) {
-        if(rooms[i].coords === player.coords) {
-            current_room = rooms[i];
+        // check the player's coordinates against every room's coordinates
+        if(isEqual(rooms[i].coords,player.coords)) {
+            current_room = rooms[i]
+        }
+    }
+}
+
+// player movement - figured this should go in with the room shit
+function goCmd(exit) {
+    exit_found = false;
+    for (let i = 0; i < current_room.exits.length; i++) {
+        if (exit === current_room.exits[i]) {
+            exit_found = true;
+            
+            // determine player's new coords - feel free to clean this up if you can do it better
+            switch(exit) {
+                case "north":
+                    player.coords[0]++;
+                    break;
+                case "south":
+                    player.coords[0]--;
+                    break;
+                case "east":
+                    player.coords[2]++;
+                    break;
+                case "west":
+                    player.coords[2]--;
+                    break;
+                case "up":
+                    player.coords[1]++;
+                    break;
+                case "down":
+                    player.coords[1]--;
+                    break;
+            }
+            output = "you moved bitch";
+            getCurrentRoom();
+        }
+
+        if(!exit_found) {
+            output = "You can't go that way.";
         }
     }
 }
