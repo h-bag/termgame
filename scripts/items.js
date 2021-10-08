fa0068b02d279582645de744039a4a0b9ad2fdc2
h-bag <30416854+h-bag@users.noreply.github.com>
2021-10-08 02:56:07 +0100

    Start work on items

---
+++ scripts/items.js
@@ -0,0 +1,18 @@
+// items n shit go in here, obviously
+
+
+// generic item class
+class Item{
+    constructor(name, desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+}
+
+// cyberdeck class
+class Cyberdeck{
+    constructor(name, desc) { // add more to this later, im just going to bed now
+        this.name = name;     // it's a placeholder
+        this.desc = desc;
+    }
+}
